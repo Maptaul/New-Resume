9e74bea11348f94849ef531998e0599ce36e3af7
Maptaul <maptaul912@gmail.com>
2025-09-19 10:38:16 +0600

    Update meta tags in index.html for improved SEO and add TypeScript and Next.js skills to Skills component

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { Outlet } from 'react-router';
+import { Outlet } from "react-router";
 
 const Root = () => {
-    return (
-        <div>
-            <Outlet />
-        </div>
-    );
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default Root;
